225ed16217b60aa14a9cbf4eb635625252758bc2
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-20 10:36:46 -0400

    Add Homework File
     Hw2-2

---
+++ hw2-2.py
@@ -0,0 +1,13 @@
+# author: LM (AMDG) 9/20/2021
+
+# free throws, baskets inside 3 point line, and baskets outside 3 point line are subject to change 
+freethrow = 1
+
+basket_inside_3point_line = 2
+     
+basket_beyond = 3
+
+total = freethrow  + basket_inside_3point_line * 2  + basket_beyond * 3
+
+
+print ( total )
