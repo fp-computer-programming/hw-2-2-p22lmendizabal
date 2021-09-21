e2d3b10228a56e74f5976c484ee1c08d233283a0
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-21 10:20:33 -0400

    add file
    Edited hw2.2

--- hw2-2.py
+++ hw2-2.py
@@ -1,13 +1,13 @@
 # author: LM (AMDG) 9/20/2021
 
 # free throws, baskets inside 3 point line, and baskets outside 3 point line are subject to change 
-freethrow = 1
+freethrow = input("What is the total number of free throws? ")
 
-basket_inside_3point_line = 2
+basket_inside_3point_line = input( "What is the total number of baskets inside the three point line? ")
      
-basket_beyond = 3
+basket_beyond = input("What is the toal number of baskets outside the three point line? ")
 
-total = freethrow  + basket_inside_3point_line * 2  + basket_beyond * 3
+total = int(freethrow)  + int(basket_inside_3point_line) * 2  + int(basket_beyond) * 3
 
 
-print ( total )
+print( "The player scored " + str(total) + " points") 
